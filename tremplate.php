7a3d6589eeaf2e8ac44306b554e3860fd29319be
adk <rouhehesoufeye@gmail.com>
2026-04-24 20:45:58 +0100

    premier commmit

---
+++ tremplate.php
@@ -0,0 +1,6 @@
+<form method="POST" action="bonjour.php">
+	<label>entrer le nom :</label>
+	<input type="text" name="nom" placeholder="votre nom "/>
+	<input type="submit" value="envoyer" />
+</form>
+//Implémentez vos bases de données relationnelles avec SQL(cour sur oppenclasrom)
